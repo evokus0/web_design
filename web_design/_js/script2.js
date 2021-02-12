a4a0b8388e47e8d5af370c95b42dd90a22ada1a6
fanedus <62357745+fanedus@users.noreply.github.com>
2021-02-11 17:36:24 -0800

    first dump
    first commit with new repository

---
+++ web_design/_js/script2.js
@@ -0,0 +1,15 @@
+
+// let choices = ["rock", "paper", "scissors"]
+
+
+// function rand_choice(x) {
+//     return Math.floor(Math.random()*x);
+// }
+
+// let cpu_choice = rand_choice(choices.length);
+// let player_choice = rand_choice(choices.length);
+
+
+
+// console.log(choices[cpu_choice]);
+// console.log(choices[player_choice]);
